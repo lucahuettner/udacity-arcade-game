9ca7169370ae04f832444b09e174e8e3be463400
Luca Huettner <luca.huettner@gmail.com>
2018-01-22 15:21:24 +0100

    added Player and Enemy class

--- js/app.js
+++ js/app.js
@@ -1,12 +1,17 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+class Enemy {
+  constructor() {
     this.sprite = 'images/enemy-bug.png';
-};
+  }
+}
+// var Enemy = function() {
+//     // Variables applied to each of our instances go here,
+//     // we've provided one for you to get started
+//
+//     // The image/sprite for our enemies, this uses
+//     // a helper we've provided to easily load images
+//     this.sprite = 'images/enemy-bug.png';
+// };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -24,12 +29,25 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+class player {
+  constructor() {
+    
+  }
+}
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+let allEnemies = [];
+let player = {
+  update: function () {
 
+  },
+  render: function () {
+
+  }
+}
 
 
 // This listens for key presses and sends the keys to your
