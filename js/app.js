9c56d1d61ec71bf21abb4efc5a9f6759720c182f
Luca Huettner <luca.huettner@gmail.com>
2018-01-23 14:08:40 +0100

    test enemy that moves

--- js/app.js
+++ js/app.js
@@ -1,7 +1,22 @@
 // Enemies our player must avoid
 class Enemy {
-  constructor() {
+  constructor(x, y) {
     this.sprite = 'images/enemy-bug.png';
+    this.x = x;
+    this.y = y;
+    this.speed = 100 + Math.floor(Math.random() * 150);
+  }
+  // Update the enemy's position, required method for game
+  // Parameter: dt, a time delta between ticks
+  update (dt) {
+    this.x += this.speed*dt
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+  }
+  // Draw the enemy on the screen, required method for game
+  render () {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
   }
 }
 // var Enemy = function() {
@@ -13,25 +28,13 @@ class Enemy {
 //     this.sprite = 'images/enemy-bug.png';
 // };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-class player {
+class Player {
   constructor() {
-    
+
   }
 }
 
@@ -40,6 +43,8 @@ class player {
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 let allEnemies = [];
+const enemy = new Enemy(100, 404);
+allEnemies.push(enemy)
 let player = {
   update: function () {
 
